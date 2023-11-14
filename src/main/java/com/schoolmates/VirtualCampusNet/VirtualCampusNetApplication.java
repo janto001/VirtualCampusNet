23b3a0df860dc544f62666097290d906f7b9cc75
anto.jayaraj <anto.jayaraj@STS-2691.sts.co.in>
2023-11-14 15:43:47 +0530

    initail commit

---
+++ src/main/java/com/schoolmates/VirtualCampusNet/VirtualCampusNetApplication.java
@@ -0,0 +1,13 @@
+package com.schoolmates.VirtualCampusNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCampusNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualCampusNetApplication.class, args);
+	}
+
+}
