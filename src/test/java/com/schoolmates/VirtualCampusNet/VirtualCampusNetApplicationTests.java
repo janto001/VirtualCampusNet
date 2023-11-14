f6db5b00f4260b26af40be84879fe19107be14e2
anto.jayaraj <anto.jayaraj@STS-2691.sts.co.in>
2023-11-14 15:44:09 +0530

    initial commit-2

---
+++ src/test/java/com/schoolmates/VirtualCampusNet/VirtualCampusNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolmates.VirtualCampusNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCampusNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
